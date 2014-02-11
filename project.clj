d3954c6bf280f65d29f8053d57abed35d1dbe38d
Ted Coakley <ted.coakley@aviso.io>
2014-02-11 12:21:21 +0000

    Release version 0.1.3

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject io.aviso/rook "0.1.3-SNAPSHOT"
+(defproject io.aviso/rook "0.1.3"
   :description "Ruby on Rails-style resource mapping for Clojure/Compojure web apps"
   :url "https://github.com/AvisoNovate/rook"
   :license {:name "Apache Sofware Licencse 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
   ;; Normally we don't AOT compile; only when tracking down reflection warnings.
   :profiles {:reflection-warnings {:aot         :all
-                                   :global-vars {*warn-on-reflection* true}}}
+                                   :global-vars {*warn-on-reflection* true}}
+             :dev {:dependencies [[ring-mock "0.1.5"]]}}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [compojure "1.1.6"]]
-  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]]}}
   :plugins [[test2junit "1.0.1"]])
