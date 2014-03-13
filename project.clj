f8e23be81a675985ddc646baffca1010c269521c
Howard M. Lewis Ship <hlship@gmail.com>
2014-03-13 14:10:18 -0700

    Fix typo

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject io.aviso/rook "0.1.7-SNAPSHOT"
   :description "Ruby on Rails-style resource mapping for Clojure/Compojure web apps"
   :url "https://github.com/AvisoNovate/rook"
-  :license {:name "Apache Sofware Licencse 2.0"
+  :license {:name "Apache Sofware License 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
   ;; Normally we don't AOT compile; only when tracking down reflection warnings.
   :profiles {:reflection-warnings {:aot         :all
