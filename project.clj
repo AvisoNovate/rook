557cb1f2cea8e82de5e5dbdbbbf2e18636ac1fe2
Howard M. Lewis Ship <hlship@gmail.com>
2014-04-03 13:46:10 -0700

    Straighten out some minor dependency conflicts

--- project.clj
+++ project.clj
@@ -6,16 +6,17 @@
   ;; Normally we don't AOT compile; only when tracking down reflection warnings.
   :profiles {:reflection-warnings {:aot         :all
                                    :global-vars {*warn-on-reflection* true}}
-             :dev {:dependencies [[ring-mock "0.1.5"]
-                                  [io.aviso/pretty "0.1.10"]
-                                  [clj-http "0.9.1"]
-                                  [speclj "2.5.0"]
-                                  [log4j "1.2.17"]]}}
+             :dev                 {:dependencies [[ring-mock "0.1.5"]
+                                                  [io.aviso/pretty "0.1.10"]
+                                                  [clj-http "0.9.1"]
+                                                  [speclj "2.5.0"]
+                                                  [log4j "1.2.17"]]}}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [ring-middleware-format "0.3.2"]
-                 [prismatic/schema "0.2.1"]
+                 [ring-middleware-format "0.3.2" :exclusions [cheshire
+                                                              org.clojure/tools.reader]]
+                 [prismatic/schema "0.2.1" :exclusions [potemkin]]
                  [compojure "1.1.6"]]
   :plugins [[speclj "2.5.0"]]
   :test-paths ["spec"])
