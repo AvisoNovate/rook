6e3032defdfb673ac9e69294c3dc596c1349927b
Howard M. Lewis Ship <hlship@gmail.com>
2014-04-06 08:09:27 -0700

    Rebuild utils/response around ring.utils.response

--- src/io/aviso/rook/utils.clj
+++ src/io/aviso/rook/utils.clj
@@ -4,6 +4,7 @@
     (java.util UUID)
     (javax.servlet.http HttpServletResponse))
   (:require
+    [ring.util.response :as r]
     [clojure.pprint :as pprint]))
 
 (defn new-uuid
@@ -14,8 +15,10 @@
 
 (defn response
   "Construct Ring response for success or other status."
-  ([body] (response HttpServletResponse/SC_OK body))
-  ([status body] {:status status :body body}))
+  ([body] (r/response body))
+  ([status body] (->
+                   (response body)
+                   (r/status status))))
 
 (defn pretty-print
   "Pretty-prints the supplied object to a returned string."
@@ -28,5 +31,5 @@
   "Prints the object more briefly, with limits on level and length."
   [object]
   (binding [*print-level* 4
-                     *print-length* 5] (pretty-print object)))
+            *print-length* 5] (pretty-print object)))
 
