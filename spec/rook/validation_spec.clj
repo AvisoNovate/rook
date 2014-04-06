a15cbf908474ecf21d963533858bfe2f6334cf70
Howard M. Lewis Ship <hlship@gmail.com>
2014-04-06 07:41:30 -0700

    Add tests related to validation in an async pipeline

--- spec/rook/validation_spec.clj
+++ spec/rook/validation_spec.clj
@@ -6,6 +6,7 @@
     io.aviso.rook
     io.aviso.rook.schema-validation)
   (:require
+    [clojure.core.async :refer [<!!]]
     [schema.core :as s]
     [clojure.tools.logging :as l]
     [io.aviso.rook.async :as async]))
@@ -32,6 +33,13 @@
           (should= HttpServletResponse/SC_BAD_REQUEST (:status response))
           (should= "validation-error" (-> response :body :error))))
 
+    (it "should handle s/enum schema types"
+        ;; Test is necessary because the s/enum type, EnumSchema, is also IPersistentMap
+        ;; with key :vs which causes a failure treating the string (or keyword) from the data as
+        ;; a map as well.
+        (should-be-nil
+          (validate-against-schema {:params {:enable "true"}} {:enable (s/enum "true" "false")})))
+
     (it "ignores extra keys"
         (should-be-nil
           (validate-against-schema {:params {:name "Rook" :ssn "999-99-9999"}} example-schema))))
@@ -44,8 +52,32 @@
           (should= HttpServletResponse/SC_BAD_REQUEST
                    (->
                      (request :post "/")
-                     (assoc :data {:first-name "Wrong Key"})
+                     (assoc :params {:first-name "Wrong Key"})
                      handler
+                     :status)))))
+
+  (describe "async middleware"
+
+    (let [handler (async/namespace-handler 'validating)]
+      (it "is present in the default async pipeline"
+          (should= HttpServletResponse/SC_BAD_REQUEST
+                   (->
+                     (request :post "/")
+                     (assoc :params {:first-name "Wrong Key"})
+                     handler
+                     <!!
+                     :status)))
+
+      (it "does not interfere with valid request"
+
+          (should= HttpServletResponse/SC_OK
+                   (->
+                     (request :post "/")
+                     (assoc :params {:name "Provided"})
+                     handler
+                     <!!
                      :status))))))
+
+
 
 (run-specs)
