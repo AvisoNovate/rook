d9b17767d47532f9616472da89111374e9e564f6
Howard M. Lewis Ship <hlship@gmail.com>
2014-05-14 09:32:10 -0700

    Update speclj dependency to 3.0.2

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -2,7 +2,10 @@
   (:use
     clojure.repl
     io.aviso.repl
-    speclj.config))
+    speclj.config)
+  (:require
+    ;; See https://github.com/slagyr/speclj/issues/79
+    speclj.run.standard))
 
 (install-pretty-exceptions)
 
--- project.clj
+++ project.clj
@@ -4,11 +4,12 @@
   :license {:name "Apache Sofware License 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
   ;; Normally we don't AOT compile; only when tracking down reflection warnings.
-  :profiles {:dev                 {:dependencies [[ring-mock "0.1.5"]
-                                                  [io.aviso/pretty "0.1.10"]
-                                                  [clj-http "0.9.1"]
-                                                  [speclj "2.5.0"]
-                                                  [log4j "1.2.17"]]}}
+  :profiles {:dev
+              {:dependencies [[ring-mock "0.1.5"]
+                              [io.aviso/pretty "0.1.10"]
+                              [clj-http "0.9.1"]
+                              [speclj "3.0.2"]
+                              [log4j "1.2.17"]]}}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [org.clojure/tools.logging "0.2.6"]
@@ -17,5 +18,5 @@
                                                               org.clojure/tools.reader]]
                  [prismatic/schema "0.2.1" :exclusions [potemkin]]
                  [compojure "1.1.6"]]
-  :plugins [[speclj "2.5.0"]]
+  :plugins [[speclj "3.0.2"]]
   :test-paths ["spec"])
