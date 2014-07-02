a516314f9a2d625b0e3b1bfaa14061d23ce0c17c
Howard M. Lewis Ship <hlship@gmail.com>
2014-07-02 16:55:50 -0700

    Clean up dependencies

--- project.clj
+++ project.clj
@@ -6,17 +6,23 @@
   :profiles {:dev
               {:dependencies [[ring-mock "0.1.5"]
                               [io.aviso/pretty "0.1.11"]
-                              [clj-http "0.9.1"]
+                              [clj-http "0.9.1" :exclusions [cheshire]]
                               [speclj "3.0.2"]
                               [log4j "1.2.17"]]}}
+  ;; List "resolved" dependencies first, which occur when there are conflicts.
+  ;; We pin down the version we want, then exclude anyone who disagrees.
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [cheshire "5.3.1"]
+                 [potemkin "0.3.4"]
+                 [org.clojure/tools.reader "0.8.3"]
+
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [org.clojure/tools.logging "0.2.6"]
                  [io.aviso/tracker "0.1.0"]
-                 [ring "1.3.0"]
-                 [medley "0.4.0"]
-                 [ring-middleware-format "0.3.2"]
-                 [prismatic/schema "0.2.3"]]
+                 [ring "1.3.0" :exclusions [org.clojure/tools.reader]]
+                 [medley "0.5.0" :exclusions [com.keminglabs/cljx org.clojure/clojure]]
+                 [ring-middleware-format "0.3.2" :excludes [cheshire]]
+                 [prismatic/schema "0.2.3" :exclusions [potemkin]]]
   :plugins [[speclj "3.0.2"]]
   :test-paths ["spec"]
   :codox {:src-dir-uri               "https://github.com/AvisoNovate/rook/blob/master/"
