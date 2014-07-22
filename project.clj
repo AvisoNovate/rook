3b57fa4d5930b550ac9918396232374caa621663
Howard M. Lewis Ship <hlship@gmail.com>
2014-07-21 17:32:34 -0700

    Update some dependencies to latest available

--- project.clj
+++ project.clj
@@ -14,15 +14,14 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.3.1"]
                  [potemkin "0.3.4"]
-                 [org.clojure/tools.reader "0.8.3"]
-
+                 [org.clojure/tools.reader "0.8.5"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [org.clojure/tools.logging "0.3.0"]
                  [io.aviso/tracker "0.1.0"]
                  [ring "1.3.0" :exclusions [org.clojure/tools.reader]]
                  [medley "0.5.0" :exclusions [com.keminglabs/cljx org.clojure/clojure]]
                  [ring-middleware-format "0.3.2" :excludes [cheshire]]
-                 [prismatic/schema "0.2.3" :exclusions [potemkin]]]
+                 [prismatic/schema "0.2.4" :exclusions [potemkin]]]
   :plugins [[speclj "3.0.2"]]
   :test-paths ["spec"]
   :codox {:src-dir-uri               "https://github.com/AvisoNovate/rook/blob/master/"
