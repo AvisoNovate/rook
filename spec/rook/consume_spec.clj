ff7ac3851b694d16c0553e62d2ede60fa0d4ed80
Howard M. Lewis Ship <hlship@gmail.com>
2014-10-02 16:41:34 -0700

    Create consume macro for working through argument lists (or other things)

---
+++ spec/rook/consume_spec.clj
@@ -0,0 +1,90 @@
+(ns rook.consume-spec
+  "Tests for the consume macro."
+  (:use speclj.core
+        io.aviso.rook.internals))
+
+(describe "io.aviso.rook.internals/consume"
+
+  (it "allows for an empty bindings vector"
+      (should= :ok
+               (consume () [] :ok)))
+
+  ;; The following tests, which use eval, fail outside the REPL, and I don't know why yet.
+  (it "ensures that the correct number of bindings terms are provided"
+      (pending)
+      (should-throw Exception "Incorrect number of binding terms for consume."
+                    (eval '(consume () [alpha zero?] :unreachable))))
+
+  (it "ensures that :& predicate is last with no arity"
+      (pending)
+      (should-throw Exception "Expected just symbol and :& placeholder as last consume binding."
+                    (eval '(consume (range 100) [xs :& :*]))))
+
+  (it "requires a known arity"
+      (pending)
+      (should-throw Exception "Unknown arity in consume binding. Expected :one, :?, :*, or :+."
+                    (eval '(consume (range 100) [xs even? :a-bunch]))))
+
+  (it "handles :& as a special predicate that gets the remaining elements"
+      (should= {:alpha [1 2 3]
+                :beta  (range 4 20)}
+               (consume (range 1 20)
+                        [alpha #(< % 4) :*
+                         beta :&]
+                        {:alpha alpha :beta beta})))
+
+  (it "allow non-matches for :*"
+      (should= {:alpha ()
+                :beta  (range 1 20)}
+               (consume (range 1 20)
+                        [alpha even? :*
+                         beta :&]
+                        {:alpha alpha :beta beta})))
+
+  (it "is an exception if :+ matches none"
+      (should-throw Exception #"Expected to consume at least one match with :\+ arity"
+                    (consume (range 1 20)
+                             [alpha even? :+
+                              beta :&]
+                             {:alpha alpha :beta beta})))
+
+  (it "should match a single value for :one"
+      (should= {:alpha 1
+                :beta  (range 2 20)}
+               (consume (range 1 20)
+                        [alpha #(< % 4) :one
+                         beta :&]
+                        {:alpha alpha :beta beta})))
+
+  (it "allows a 1 instead of :one"
+      (should= {:alpha 1
+                :beta  (range 2 20)}
+               (consume (range 1 20)
+                        [alpha #(< % 4) 1
+                         beta :&]
+                        {:alpha alpha :beta beta})))
+
+  (it "is an exception if :one does not match"
+      (should-throw Exception #"consume :one arity did not match"
+                    (consume (range 1 20)
+                             [alpha even? :one
+                              beta :&]
+                             {:alpha alpha :beta beta})))
+
+  (it "should match a single value for :?"
+      (should= {:alpha 1
+                :beta  (range 2 20)}
+               (consume (range 1 20)
+                        [alpha #(< % 4) :?
+                         beta :&]
+                        {:alpha alpha :beta beta})))
+
+  (it "should bind a nil if the first collection value doesn't match with :?"
+      (should= {:alpha nil
+                :beta  (range 1 20)}
+               (consume (range 1 20)
+                        [alpha even? :?
+                         beta :&]
+                        {:alpha alpha :beta beta}))))
+
+(run-specs)
--- src/io/aviso/rook/internals.clj
+++ src/io/aviso/rook/internals.clj
@@ -70,12 +70,12 @@
   `(try
      ~@body
      (catch Throwable t#
-            (let [r# ~request]
-              (l/errorf t# "Exception processing request %s (%s)"
-                        (:request-id r# (or "<INCOMING>"))
-                        (utils/summarize-request r#)))
-            (utils/response HttpServletResponse/SC_INTERNAL_SERVER_ERROR
-                            {:exception (to-message t#)}))))
+       (let [r# ~request]
+         (l/errorf t# "Exception processing request %s (%s)"
+                   (:request-id r# (or "<INCOMING>"))
+                   (utils/summarize-request r#)))
+       (utils/response HttpServletResponse/SC_INTERNAL_SERVER_ERROR
+                       {:exception (to-message t#)}))))
 
 (defmacro safe-go
   "Wraps the body in a [[safety-first]] block and then in a go block. The request is used by [[safety-first]] if it must
@@ -144,3 +144,93 @@
         `(if ~(first forms)
            ~(second forms)
            (cond-let ~@(drop 2 forms)))))))
+
+(defmacro consume
+  "Consume is used to break apart a collection into individual terms.  The format is
+
+      (consume coll [symbol pred arity ...] body)
+
+  The symbol is assigned a value by extracting zero or more values from the collection that
+  match the predicate. The arity is a keyword that identifies how many values are taken
+  from the collection.
+
+  :one
+  : The first value in the collection must match the predicate, or an exception is thrown.
+    The value is assigned to the symbol.  The literal value 1 may be used instead of :one.
+
+  :?
+  : Matches 0 or 1 values from the collection. If the first value does not match the predicate,
+    then nil is assigned to the symbol.
+
+  :*
+  : Zero or more values from the collection are assigned to the symbol. The symbol may be assigned
+    an empty collection.
+
+  :+
+  : Matches one or more values; an exception is thrown if there are no matches.
+
+  The symbol/pred/arity triplet can be followed by additional triplets.
+
+  The special predicate :& is used to indicate consumption of all remaining values, if any, from the collection.
+  It is not followed by an arity, and must be the final term in the bindings vector.
+
+  consume expands into a let form, so the symbol in each triplet may be a destructuring form."
+  [coll bindings & body]
+  (let [[symbol pred arity] bindings]
+    (cond-let
+      [binding-count (count bindings)]
+
+      (zero? binding-count)
+      `(do ~@body)
+
+      (= pred :&)
+      (do
+        (if-not (= 2 binding-count)
+          (throw (ex-info "Expected just symbol and :& placeholder as last consume binding."
+                          {:symbol   symbol
+                           :pred     pred
+                           :bindings bindings})))
+        `(let [~symbol ~coll] ~@body))
+
+      (< binding-count 3)
+      (throw (ex-info "Incorrect number of binding terms for consume."
+                      {:bindings bindings}))
+
+      [remaining-bindings (drop 3 bindings)]
+
+      :else
+      (case arity
+
+        (1 :one)
+        `(let [coll# ~coll
+               first# (first coll#)]
+           (if-not (~pred first#)
+             (throw (ex-info "consume :one arity did not match"
+                             {:symbol (quote ~symbol)
+                              :pred   (quote ~pred)}))
+             (let [~symbol first#]
+               (consume (rest coll#) ~remaining-bindings ~@body))))
+
+        :?
+        `(let [coll# ~coll
+               first# (first coll#)
+               match# (~pred first#)]
+           (let [~symbol (if match# first# nil)]
+             (consume (if match# (rest coll#) coll#) ~remaining-bindings ~@body)))
+
+        :*
+        `(let [[~symbol remaining#] (split-with ~pred ~coll)]
+           (consume remaining# ~remaining-bindings ~@body))
+
+        :+
+        `(let [[matching# remaining#] (split-with ~pred ~coll)]
+           (if (empty? matching#)
+             (throw (ex-info "Expected to consume at least one match with :+ arity"
+                             {:symbol (quote ~symbol)
+                              :pred   (quote ~pred)})))
+           (let [~symbol matching#]
+             (consume remaining# ~remaining-bindings ~@body)))
+
+        (throw (ex-info "Unknown arity in consume binding. Expected :one, :?, :*, or :+."
+                        {:bindings bindings
+                         :body     body}))))))
