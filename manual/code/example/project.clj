c8218948b26057eeaf4fe963f3a7f443d4c047ba
Howard M. Lewis Ship <hlship@gmail.com>
2014-11-12 12:13:00 -0800

    Update dependencies

--- manual/code/example/project.clj
+++ manual/code/example/project.clj
@@ -6,5 +6,5 @@
             :plugins [[speclj "3.1.0"]]
             :test-paths ["spec"]
             :profiles {:dev {:dependencies [[speclj "3.1.0"]
-                                            [clj-http "1.0.0"]
+                                            [clj-http "1.0.1"]
                                             [log4j "1.2.17"]]}})
