39b5cea9dacdde50672f90df59621a0b58ce3e94
Howard M. Lewis Ship <hlship@gmail.com>
2014-11-25 13:38:06 -0800

    A few tweaks to the manual

--- manual/code/example/project.clj
+++ manual/code/example/project.clj
@@ -2,7 +2,7 @@
             :description "An example rook resource"
             :main org.example.server/main
             :dependencies [[org.clojure/clojure "1.6.0"]
-                           [io.aviso/rook "0.1.17"]]
+                           [io.aviso/rook "0.1.18"]]
             :plugins [[speclj "3.1.0"]]
             :test-paths ["spec"]
             :profiles {:dev {:dependencies [[speclj "3.1.0"]
