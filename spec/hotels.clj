4bab5ee02efed0b8bc3bc259bfd88dfd6127e6c6
Howard M. Lewis Ship <hlship@gmail.com>
2015-04-10 16:48:04 -0700

    Start filling in descriptions of responses

--- spec/hotels.clj
+++ spec/hotels.clj
@@ -19,7 +19,7 @@
    (s/optional-key :descending) s/Bool})
 
 (def index-responses
-  {HttpServletResponse/SC_OK [Hotel]})
+  {HttpServletResponse/SC_OK (rs/schema HotelList "List of matching hotels in specified order." [Hotel])})
 
 (defn index
   "Returns a list of all hotels, with control over sort order."
@@ -29,8 +29,8 @@
   nil)
 
 (def show-responses
-  {HttpServletResponse/SC_OK        Hotel
-   HttpServletResponse/SC_NOT_FOUND nil})
+  {HttpServletResponse/SC_OK        (rs/with-description "The hotel matching the id, if found." Hotel)
+   HttpServletResponse/SC_NOT_FOUND (rs/description "No hotel with the provided id could be found.")})
 
 (defn show
   "Returns a single hotel, if found."
@@ -40,12 +40,13 @@
 
 (rs/defschema ChangeBody
   "Allows the name of a hotel to be changed (when doing so does not conflict with an existing hotel)."
-  {:name s/Str})
+  {:name (rs/with-description "The new name for the hotel, which must be unique."
+                              s/Str)})
 
 (def change-responses
-  {HttpServletResponse/SC_NOT_FOUND nil
-   HttpServletResponse/SC_CONFLICT  nil
-   HttpServletResponse/SC_OK        nil})
+  {HttpServletResponse/SC_NOT_FOUND (rs/description "No hotel with the provided id was located.")
+   HttpServletResponse/SC_CONFLICT  (rs/description "The hotel can not be renamed as another hotel has the same name.")
+   HttpServletResponse/SC_OK        (rs/description "The hotel was succesfully updated.")})
 
 (defn change
   "Updates a Hotel, to rename it. May result in a 409 Conflict if some other hotel has the same name."
