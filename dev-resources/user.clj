033a79ccf594899ca3fce2e43c7762a029d949fa
Howard M. Lewis Ship <hlship@gmail.com>
2015-04-10 10:07:06 -0700

    Enable pretty hooks for logging

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -2,8 +2,11 @@
   (:use
     clojure.repl
     io.aviso.repl
+    io.aviso.logging
     speclj.config))
 
 (install-pretty-exceptions)
+(install-pretty-logging)
+(install-uncaught-exception-handler)
 
 (alter-var-root #'default-config assoc :color true :reporters ["documentation"])
