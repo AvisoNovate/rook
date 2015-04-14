f79113ae9c217c4fc07039b1cd42a5172686d119
Howard M. Lewis Ship <hlship@gmail.com>
2015-04-14 09:53:26 -0700

    Terminate stack listings when the speclj functions are reached

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -2,6 +2,7 @@
   (:use
     clojure.repl
     io.aviso.repl
+    io.aviso.exception
     io.aviso.logging
     speclj.config))
 
@@ -10,3 +11,10 @@
 (install-uncaught-exception-handler)
 
 (alter-var-root #'default-config assoc :color true :reporters ["documentation"])
+
+(alter-var-root #'*default-frame-filter*
+                (fn [default]
+                  (fn [frame]
+                    (if (-> frame :name (.startsWith "speclj."))
+                      :terminate
+                      (default frame)))))
