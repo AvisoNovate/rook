b73e4fb00f175da263d1f5ca5ee65ab5e9487219
Howard M. Lewis Ship <hlship@gmail.com>
2015-04-14 10:18:20 -0700

    Add basic CORS enabling headers

--- src/io/aviso/rook/resources/swagger.clj
+++ src/io/aviso/rook/resources/swagger.clj
@@ -8,12 +8,19 @@
 
 (defn swagger-json
   "Returns the Swagger API description as (pretty) JSON."
-  {:route [:get "swagger.json"]}
+  {:route [:get ["swagger.json"]]}
   [swagger-object]
   ;; It's a bit silly to stream this to JSON on each request; a cache would be nice. Later.
   ;; Don't want to rely on outer layers providing the right middleware, so we do the conversion
   ;; to JSON right here.
+  ;; More to do w.r.t. CORS and enabling the client to submit requests.
   (-> swagger-object
       (json/generate-string {:pretty true})
       res/response
+      ;; Access-Control-Allow-Origin: *
+      ;; Access-Control-Allow-Methods: GET, POST, DELETE, PUT, PATCH, OPTIONS
+      ;; Access-Control-Allow-Headers: Content-Type, api_key, Authorization
+      (res/header "Access-Control-Allow-Origin" "*")
+      (res/header "Access-Control-Allow-Methods" "GET")
+      (res/header "Access-Control-Allow-Headers" "Content-Type")
       (res/content-type "application/json")))
