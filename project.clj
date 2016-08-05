6472e5ac7039228126df69fae5137f6a5b9668b2
Howard M. Lewis Ship <hlship@walmartlabs.com>
2016-08-05 10:28:32 -0700

    Fix broken project.clj

--- project.clj
+++ project.clj
@@ -3,12 +3,11 @@
   :url "https://github.com/AvisoNovate/rook"
   :license {:name "Apache Sofware License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :profiles :dev
-  {:dependencies [[ring-mock "0.1.5"]
-                  [cc.qbits/jet "0.7.10"]
-                  [speclj "3.3.2"]
-                  [io.aviso/logging "0.1.0"]
-                  [clj-http "3.1.0"]]}
+  :profiles {:dev {:dependencies [[ring-mock "0.1.5"]
+                             [cc.qbits/jet "0.7.10"]
+                             [speclj "3.3.2"]
+                             [io.aviso/logging "0.1.0"]
+                             [clj-http "3.1.0"]]}}
   ;; List "resolved" dependencies first, which occur when there are conflicts.
   ;; We pin down the version we want, then exclude anyone who disagrees.
   :dependencies [[org.clojure/clojure "1.7.0"]
