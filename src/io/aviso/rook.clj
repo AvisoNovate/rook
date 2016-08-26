9d7dddddcc54667561e09484d6248acc3aed10c6
Howard M. Lewis Ship <hlship@walmartlabs.com>
2016-08-25 17:02:47 -0700

    Rename and doument some useful functions and make them public

--- src/io/aviso/rook.clj
+++ src/io/aviso/rook.clj
@@ -9,23 +9,43 @@
 ;; Remember that a parameter is the symbol defined by a function, and an argument
 ;; is the actual value passed to the function.
 
-(defn ^:private non-nil-arg-resolver
+(defn non-nil-arg-resolver
+  "Returns an arg resolver that extracts a value nested in the context.
+
+  The value must be non-nil, or an exception is thrown.
+
+  sym
+  : The symbol for which the argument resolver was created; identified in the exception.
+
+  ks
+  : a sequence of keys."
   [sym ks]
   (fn [context]
     (let [v (get-in context ks)]
       (if (some? v)
         v
         (throw (ex-info "Resolved argument value was nil."
-                        {:parameter sym
+                        {:type ::argument-nil
+                         :parameter sym
                          :context-key-path ks}))))))
 
-(defn ^:private standard-arg-resolver
+(defn standard-arg-resolver
+  "Returns an arg resolver that extracts a value nested in the context.
+
+  ks
+  : A sequence of keys."
   [ks]
   (fn [context]
     (get-in context ks)))
 
 
-(defn ^:private from-meta
+(defn meta-value-for-parameter
+  "Given a parameter symbol and a metadata key, returns the value for that metadata key.
+
+  It is presumed that the value is a keyword, though no check is made.
+
+  When the metadata value is explicitly true, the symbol is converted into a simple (not namespaced)
+  keyword."
   [sym meta-key]
   (let [meta-value (-> sym meta (get meta-key))]
     ;; This catches the typical default cause where the meta value is true,
@@ -37,20 +57,20 @@
 
 (defn ^:private request-resolver
   [sym]
-  (non-nil-arg-resolver sym [:request (from-meta sym :request)]))
+  (non-nil-arg-resolver sym [:request (meta-value-for-parameter sym :request)]))
 
 (defn ^:private path-param-resolver
   [sym]
-  (non-nil-arg-resolver sym [:request :path-params (from-meta sym :path-param)]))
+  (non-nil-arg-resolver sym [:request :path-params (meta-value-for-parameter sym :path-param)]))
 
 (defn ^:private query-param-resolver
   [sym]
   (standard-arg-resolver
-    [:request :query-params (from-meta sym :query-param)]))
+    [:request :query-params (meta-value-for-parameter sym :query-param)]))
 
 (defn ^:private form-param-resolver
   [sym]
-  (standard-arg-resolver [:request :form-params (from-meta sym :form-param)]))
+  (standard-arg-resolver [:request :form-params (meta-value-for-parameter sym :form-param)]))
 
 (def default-arg-resolvers
   "Defines the following argument resolvers:
