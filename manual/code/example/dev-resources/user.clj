73773fd818006d65a331ab1112a278552f937a0c
Howard M. Lewis Ship <hlship@walmartlabs.com>
2016-08-26 09:30:33 -0700

    Start work on Rook 0.2 docs

--- manual/code/example/dev-resources/user.clj
+++
@@ -1,9 +0,0 @@
-(ns user
-  (:use
-    clojure.repl
-    io.aviso.repl
-    speclj.config))
-
-(install-pretty-exceptions)
-
-(alter-var-root #'default-config assoc :color true :reporters ["documentation"])
--- manual/code/example/minimal-project.clj
+++
@@ -1,7 +0,0 @@
-(defproject rook-example "0.0.1"
-    :description "An example rook resource"
-    :main org.example.server/main
-    :dependencies [
-        [org.clojure/clojure "1.6.0"]
-        [io.aviso/rook "0.1.21"]
-        [ring "1.3.2"]])
